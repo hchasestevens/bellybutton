f0c7e88217bd978d1648e4cbc6fae11a6be4f919
H. Chase Stevens <chase@chasestevens.com>
2018-02-24 00:37:06 -0500

    Add outline for CLI commands (#4)

--- bellybutton/cli.py
+++ bellybutton/cli.py
@@ -1,1 +1,54 @@
 """Command-line interface."""
+
+import sys
+import argparse
+
+try:
+    from itertools import zip_longest
+except ImportError:
+    from itertools import izip_longest as zip_longest
+
+
+PARSER = argparse.ArgumentParser()
+SUBPARSERS = PARSER.add_subparsers()
+
+
+def cli_command(fn):
+    """Register function as subcommand."""
+    command = SUBPARSERS.add_parser(fn.__name__, description=fn.__doc__)
+    unspecified = object()
+    args = reversed(list(zip_longest(
+        reversed(fn.__code__.co_varnames),
+        reversed(fn.__defaults__),
+        fillvalue=unspecified
+    )))
+    for argument_name, default_value in args:
+        if default_value is unspecified:
+            command.add_argument(argument_name, required=True)
+            continue
+        command.add_argument(
+            '--{}'.format(argument_name),
+            default=default_value,
+            required=False
+        )
+    command.set_defaults(func=fn)
+    return fn
+
+
+@cli_command
+def init(project_directory='.'):
+    """Intiialize bellybutton config for project."""
+
+
+@cli_command
+def lint(level='all', project_directory='.'):
+    """Lint project."""
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    ARGS = PARSER.parse_args()
+    ARGS.func(**{
+        arg: getattr(ARGS, arg)
+        for arg in ARGS.func.__code__.co_varnames
+    })
